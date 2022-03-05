575026f11349ab1b1aa146669f3f22dc10b088dd
graciari2000 <ariane-gracia.akakpo@epitech.eu>
2022-03-05 19:32:44 +0100

    some code i added

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => (runApp(CalculatorApp));
+void main() => runApp(CalculatorApp());
 
 class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
     );
   }
+}
+
+class Calculate extends StatefulWidget {
+  @override
+  _CalculateState createState() => _CalculateState();
+}
+
+class _CalculateState extends State<Calculate> {
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+  backgroundColor: Colors.black,
+  appBar: AppBar(title: Text('Calculator'), backgroundColor: Colors.black,),
+  body: Padding(
+    padding: EdgeInsets.symmetric(horizontal: 5), 
+  child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: ,
+            )
+            ],
+            ),)
+            )
 }
